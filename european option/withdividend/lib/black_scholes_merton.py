1bcc6f5d0d4c184fc08b654b5fb4fd03f9b9b349
Mahfuz Islam Suave <90802333+suavie@users.noreply.github.com>
2024-05-19 21:31:33 +0600

    Add files via upload

---
+++ european option/withdividend/lib/black_scholes_merton.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.stats import norm
+
+def black_scholes_merton(S, K, T, r, sigma, option_type='call'):
+    T=T/252
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    if option_type == 'call':
+        option_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    elif option_type == 'put':
+        option_price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+    else:
+        raise ValueError("Invalid option type. Use 'call' or 'put'.")
+
+    return option_price
+
